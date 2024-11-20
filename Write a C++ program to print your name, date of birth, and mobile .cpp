6301ff7baf46907d164dde2582fc21c72d08a3fb
LITON ALI RIFAT <litonalirifat224@gmail.com>
2024-11-20 11:48:59 +0600

    Add files via upload

---
+++ Write a C++ program to print your name, date of birth, and mobile .cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name,birth,mobile;
+    cout<<"Enter your name :" <<endl;
+    cin>>name;
+    cout<<"Enter your date of birth :"<<endl;
+    cin>>birth;
+    cout<<"Enter your mobile number :"<<endl;
+    cin>>mobile;
+    cout<<"your name is ":<<name<<endl;
+    cout<<"your date of birth is :"<<birth<<endl;
+    cout<<"your mobile number is :"<<mobile<<endl;
+
+
+return 0;
+
+}
