f61f415ff2fb1ba4ac27bc55b7657677df1f77da
LITON ALI RIFAT <litonalirifat224@gmail.com>
2024-11-20 12:04:26 +0600

    Add files via upload

---
+++ Write a C++ program to compute the perimeter and area of a circle with a given radius.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int area,perimeter,redius;
+    cout<<"Enter the redius of circle in meter:";
+    cin>>redius;
+    perimeter=2*3.1416*redius;
+    area=3.1416*redius*redius;
+    cout<<"The area of the tringle is: "<<area<<"meter "<<endl;
+    cout<<"The perimeter of the tringle is: "<<perimeter<<"meter "<<endl;
+
+}
