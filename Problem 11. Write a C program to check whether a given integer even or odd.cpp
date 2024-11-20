b149805562979c597eec765ad202721447d67888
LITON ALI RIFAT <litonalirifat224@gmail.com>
2024-11-20 14:48:37 +0600

    Add files via upload

---
+++ Problem 11. Write a C program to check whether a given integer even or odd.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int password,pass;
+    password=2107111;
+    cout<<"enter password : ";
+    cin>>pass;
+    if(password==pass)
+    {
+        cout<<"Correct passsword";
+    }
+    else
+    {
+        cout<<"Incorrect password";
+    }
+return 0;
+}
+
