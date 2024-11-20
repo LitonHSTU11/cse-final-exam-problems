2ae26d60f01560b237e8357c8fc99c5d61eeb78f
LITON ALI RIFAT <litonalirifat224@gmail.com>
2024-11-20 13:10:52 +0600

    Add files via upload

---
+++ Write a C program that reads an integer between 1 and 12 and prints the month of the year in English.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter the no. of month : ";
+    cin>>a;
+    if(a==1)
+    {
+        cout<<"January";
+    }
+    else if(a==2)
+    {
+        cout<<"Febuary";
+    }
+    else if(a==3)
+    {
+        cout<<"march";
+    }
+    else if(a==4)
+    {
+        cout<<"april";
+    }
+     else if(a==5)
+    {
+        cout<<"may";
+    }else if(a==6)
+    {
+        cout<<"june";
+    }
+    else if(a==7)
+    {
+        cout<<"july";
+    }
+    else if(a==8)
+    {
+        cout<<"august";
+    }
+    else if(a==9)
+    {
+        cout<<"september";
+    }
+    else if(a==10)
+    {
+        cout<<"october";
+    }
+    else if(a==11)
+    {
+        cout<<"november";
+    }
+    else if(a==12)
+    {
+        cout<<"december";
+    }
+    else
+    {
+        cout<<"You press wrong number";
+    }
+  return 0;
+
+}
