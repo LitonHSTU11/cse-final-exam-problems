afa00f74d4ba3d4d4ef87a39758a00920b318775
LITON ALI RIFAT <litonalirifat224@gmail.com>
2024-11-20 12:13:53 +0600

    Add files via upload

---
+++ Write a C++ program that accepts two integers from the user.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sumation,substraction,multiplication,division;
+    cout<<"Enter the first number: ";
+    cin>>a;
+    cout<<"Enter the second number: ";
+    cin>>b;
+    sumation=a+b;
+    substraction=a-b;
+    multiplication=a*b;
+    division=a/b;
+    cout<<"The summation of two number is :"<<sumation<<endl;
+    cout<<"The substraction of two number is :"<<substraction<<endl;
+    cout<<"The multiplicatio of two number is :"<<multiplication<<endl;
+    cout<<"The division of two number is :"<<division<<endl;
+return 0;
+}
