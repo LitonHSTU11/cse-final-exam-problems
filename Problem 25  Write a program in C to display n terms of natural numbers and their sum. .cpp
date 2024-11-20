e7da3d0322ffc07d1b1c5ac9ba8e8e8ebe773994
LITON ALI RIFAT <litonalirifat224@gmail.com>
2024-11-20 17:54:10 +0600

    Add files via upload

---
+++ Problem 25  Write a program in C to display n terms of natural numbers and their sum. .cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,sum;
+    cout<<"Enter the value of n : ";
+    cin>>n;
+    cout<<"The first 10 natural number is : "<<endl;
+    for(int i=1;i<=n;i++)
+    {
+        cout<<i<<" ";
+        sum=sum+i;
+    }
+    cout<<endl;
+    cout<<"The sum"<<n<<"natural number is : "<<sum;
+return 0;
+
+}
+
